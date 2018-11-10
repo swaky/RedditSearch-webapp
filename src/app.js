090d116959e6b9f657e96a4bc88b1a204d2ec4bb
Swanand Keskar <sw4n4nd@gmail.com>
2018-11-10 16:39:55 +0530

    display reddit data using reddit api

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import reddit from './redditapi';
 
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
-
+const outputdiv=document.getElementById('results');
 
 //form event listener
 searchForm.addEventListener('submit',event=>{
@@ -29,10 +29,47 @@ searchForm.addEventListener('submit',event=>{
     searchInput.value='';
 
     //search reddit
-    reddit.search(searchTerm,searchLimit,sortby);
+    // reddit.search(searchTerm,searchLimit,sortby).then(results=>{
+    //     console.log(results);
+    
+    
+    //     outputdiv.innerHTML="<h2>api called</h2>";
+    // })
+
+    const response =  reddit.search(searchTerm,searchLimit,sortby)
+    .then(results=>{
+            console.log(results);
+
+            let output ='<div class="card-columns">';
+
+            results.forEach(post=>{
+
+                let image = post.preview ? post.preview.images[0].source.url :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNbrwanPeWtbDMetYH6TtKXH6ucQzwz2niu0sHxlFKLd5y1B2K';
+
+                output += `
+                <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="${image}" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">${post.title}</h5>
+    <p class="card-text">${post.selftext.substring(0,300)}</p>
+    <a href="${post.url}" targer="_blank" class="btn btn-primary">Read More</a>
+    <hr>
+    <span class="badge badge-secondary">Subreddit: ${post.subreddit}</span>
+    <span class="badge badge-dark">Score: ${post.score}</span>
+  </div>
+</div>
+                `;
+            })
+
+            output += "</div>";
+        
+            outputdiv.innerHTML=output;
+        })
+
 
     event.preventDefault();
 });
+
 
 //show message
 function showMessage(message,className){
--- src/redditapi.js
+++ src/redditapi.js
@@ -1,9 +1,10 @@
 export default {
     search:function(searchTerm,searchLimit,sortby){
-//         console.log("Search ...");
 
-     return    fetch(`http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortby}&limit=${searchLimit}`)
+     return  fetch(
+         `http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortby}&limit=${searchLimit}`)
          .then(res => res.json())
-         .then(data => console.log(data));
+         .then(data => data.data.children.map(data=>data.data))
+         .catch(err=>console.log(err));
     }
 }
